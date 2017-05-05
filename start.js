ccbf730b40b1c1d5dbab0788e6f065ff5e10de60
Jason Brewster <jdbrew@me.com>
2017-05-05 19:04:46 +0100

    Did you remember to (npm) install?

--- start.js
+++ start.js
@@ -1,3 +1,27 @@
+//=============================================================================
+// # Start
+//=============================================================================
+// In this file let's get the install checked and the app up and running.
+
+// Check we have installed modules
+// =============================================================================
+// Check we have a `node_modules` folder otherwise prompt an install
+//
+// Firstly the requires
+var path = require('path')
+var fs = require('fs')
+
+// Check `node_modules` folder exists...
+const nodeModulesExists = fs.existsSync(path.join(__dirname, '/node_modules'))
+// ...and prompt an install if missing
+if (!nodeModulesExists) {
+  console.error('ERROR: Node module folder missing. Try running `npm install`')
+  process.exit(0)
+}
+
+
+
+
 var express = require('express');
 var nunjucks = require('nunjucks');
 
