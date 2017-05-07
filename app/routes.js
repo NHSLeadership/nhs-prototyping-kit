137b689ebebd08ef766d715cfe1e0b9d86c5f533
Jason Brewster <jdbrew@me.com>
2017-05-07 18:21:54 +0100

    Routes.js in app
    - Routes in app/routes.js
    - Tidy up messy code

---
+++ app/routes.js
@@ -0,0 +1,30 @@
+// routes.js
+// ===========================================================================
+
+// Routes setup
+// ---------------------------------------------------------------------------
+// use Router object
+var express = require('express')
+var router = express.Router()
+
+// Set routes
+// ---------------------------------------------------------------------------
+// Home page
+router.get('/', function(req, res) {
+    res.render('index')
+});
+
+
+// CUSTOM ROUTES HERE
+// Page-name
+// router.get('/page-name', function(req, res) {
+//     res.render('page-name')
+// });
+
+
+// Export routes
+// ---------------------------------------------------------------------------
+// Do not change below this line
+//
+// Export router to use elswhere in the app
+module.exports = router
--- start.js
+++ start.js
@@ -1,14 +1,21 @@
-/** ===========================================================================
-*   # Start
-*   ===========================================================================
-*   In this file let's get the install checked and the app up and running.
-*/
+// start.js
+// ==========================================================================
+// In this file let's get the install checked and the app up and running.
 
 
-/** Check we have installed modules
-*   ---------------------------------------------------------------------------
-*   Check we have a `node_modules` folder otherwise prompt an install
-*/
+// BASE SETUP
+// --------------------------------------------------------------------------
+
+var express = require('express');
+var app = express();
+// Set port to 3000
+var port = process.env.PORT || 3000;
+app.set(port)
+
+// Check we have installed modules
+// --------------------------------------------------------------------------
+// Check we have a `node_modules` folder otherwise prompt an install
+//
 
 // Firstly the requires
 var path = require('path')
@@ -25,25 +32,18 @@ if (!nodeModulesExists) {
 
 
 
-/** Use Nunjucks
-*   ---------------------------------------------------------------------------
-*   Configure app to use nunjucks templating style to render views.
-*/
+// Use Nunjucks
+// ---------------------------------------------------------------------------
+// Configure app to use nunjucks templating style to render views.
+//
 
 // Firstly the requires
-var express = require('express');
 var nunjucks = require('nunjucks');
 
-// Set up App
-var app = express();
+// Location of views
 var appViews = [path.join(__dirname, '/app/views/')]
 
-// Setup nunjucks templating engine
-nunjucks.configure('views', {
-    autoescape: true,
-    express: app
-});
-
+// Configure nunjucks templating engine
 var nunjucksAppEnv = nunjucks.configure(appViews, {
   autoescape: true,
   express: app,
@@ -54,36 +54,19 @@ var nunjucksAppEnv = nunjucks.configure(appViews, {
 // Set views engine
 app.set('view engine', 'html')
 
+// Routes
+// ---------------------------------------------------------------------------
+// Import routes
+//
+
+app.use(require('./app/routes'))
 
 
-/** Start server
-*   ---------------------------------------------------------------------------
-*   set ports and GO
-*/
-
-// Set ports
-app.set('port', process.env.PORT || 3000);
+// Start server
+// ---------------------------------------------------------------------------
+//
 
 // Kick start our server
-app.listen(app.get('port'), function() {
-    console.log('Server started on port', app.get('port'));
-});
-
-// TODO : this needs putting in a server.js
-
-
-/** Routes
-*   ---------------------------------------------------------------------------
-*   Decide the content served from the URL
-*/
-
-// Home page
-app.get('/', function(req, res) {
-    res.render('index.html', {
-        // Pass variables
-        page: 'Home',
-        port: app.get('port')
-    });
-});
-
-// TODO : this needs putting in a routes.js
+app.listen(port);
+// Tell us it's started
+console.log('Server started on port', port);
