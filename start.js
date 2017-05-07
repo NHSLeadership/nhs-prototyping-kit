97ffddf0c35a7ef3d71825c6d4f6926a045f19c4
Jason Brewster <jdbrew@me.com>
2017-05-07 08:57:09 +0100

    Routes last
    Ports not set before server start

--- start.js
+++ start.js
@@ -1,18 +1,22 @@
-//=============================================================================
-// # Start
-//=============================================================================
-// In this file let's get the install checked and the app up and running.
+/** ===========================================================================
+*   # Start
+*   ===========================================================================
+*   In this file let's get the install checked and the app up and running.
+*/
 
-// Check we have installed modules
-// =============================================================================
-// Check we have a `node_modules` folder otherwise prompt an install
-//
+
+/** Check we have installed modules
+*   ---------------------------------------------------------------------------
+*   Check we have a `node_modules` folder otherwise prompt an install
+*/
+
 // Firstly the requires
 var path = require('path')
 var fs = require('fs')
 
 // Check `node_modules` folder exists...
 const nodeModulesExists = fs.existsSync(path.join(__dirname, '/node_modules'))
+
 // ...and prompt an install if missing
 if (!nodeModulesExists) {
   console.error('ERROR: Node module folder missing. Try running `npm install`')
@@ -21,10 +25,11 @@ if (!nodeModulesExists) {
 
 
 
-// Use Nunjucks
-// =============================================================================
-// Configure app to use nunjucks templating style to render views.
-//
+/** Use Nunjucks
+*   ---------------------------------------------------------------------------
+*   Configure app to use nunjucks templating style to render views.
+*/
+
 // Firstly the requires
 var express = require('express');
 var nunjucks = require('nunjucks');
@@ -38,29 +43,34 @@ nunjucks.configure('views', {
 });
 
 
-// Routes
-// =============================================================================
-// Decide the content served from the URL
-//
-// Home page
-app.get('/', function(req, res) {
-    res.render('index.html', {
-        page: 'home',
-        port: app.get('port')
-    });
-});
-// TODO : this needs putting in a routes.js
+/** Start server
+*   ---------------------------------------------------------------------------
+*   set ports and GO
+*/
 
-
-// Start server
-// =============================================================================
-// set ports and GO
-//
 // Set ports
 app.set('port', process.env.PORT || 3000);
+
 // Kick start our server
 app.listen(app.get('port'), function() {
     console.log('Server started on port', app.get('port'));
 });
 
 // TODO : this needs putting in a server.js
+
+
+/** Routes
+*   ---------------------------------------------------------------------------
+*   Decide the content served from the URL
+*/
+
+// Home page
+app.get('/', function(req, res) {
+    res.render('index.html', {
+        // Pass variables
+        page: 'Home',
+        port: app.get('port')
+    });
+});
+
+// TODO : this needs putting in a routes.js
